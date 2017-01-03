877f6c1bdf3ecc8a851ab75aac8f67b54d131d7f
Kieran <kieran.summerfeldt@gmail.com>
2017-01-03 15:26:41 +0200

    set up home component

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className='alert alert-info'>
+        <h1>Hello from Home Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
